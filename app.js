70e0d5ba7c07af9b427b87c6eb051f7525478710
Omar Aljazairy <omar@fedal.net>
2020-06-05 14:56:50 +0200

    adjusted the uri

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const corsOptions = {
     credentials: true,
     origin: [
         'http://127.0.0.1:3000',
-        'http://127.0.0.1'
+        'http://127.0.0.1',
+        'http://localhost'
     ],
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
 }
